9ab0bd76e724cd9d3157afd09cc038389be7817f
Augusto Bernardes <gutocbsantos@yahoo.com.br>
2022-07-30 15:08:35 -0300

    Criação de dto de comandos

---
+++ gframe/ScreenReader/CommandMessages.cpp
@@ -0,0 +1,8 @@
+#include "CommandMessages.h"
+
+std::wstring CommandMessages::ATTACK = L"attack";
+std::wstring CommandMessages::ACTIVATE = L"activate";
+std::wstring CommandMessages::CHANGE_MODE = L"change battle mode";
+std::wstring CommandMessages::NORMAL_SUMMON = L"normal summon";
+std::wstring CommandMessages::SPECIAL_SUMMON = L"special summon";
+std::wstring CommandMessages::SET_CARD = L"set";
---
+++ gframe/ScreenReader/CommandMessages.h
@@ -0,0 +1,16 @@
+#ifndef COMMANDMESSAGES_H
+#define COMMANDMESSAGES_H
+
+#include <string>
+class CommandMessages
+{
+public:
+	static std::wstring ATTACK;
+	static std::wstring ACTIVATE;
+	static std::wstring CHANGE_MODE;
+	static std::wstring NORMAL_SUMMON;
+	static std::wstring SPECIAL_SUMMON;
+	static std::wstring SET_CARD;
+
+};
+#endif

063d02b11010fbddcf22c22a4c3070f61719a6f7
Augusto Bernardes <gutocbsantos@yahoo.com.br>
2022-08-21 17:21:26 -0300

    Update image_downloader.cpp

--- gframe/image_downloader.cpp
+++ gframe/image_downloader.cpp
@@ -36,6 +36,7 @@ void ImageDownloader::AddDownloadResource(PicSource src) {
 
 static constexpr std::array<uint8_t, 8> pngheader{ 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a };
 static constexpr std::array<uint8_t, 3> jpgheader{ 0xff, 0xd8, 0xff };
+static int numberImagesDownloaded = 0;
 
 enum headerType : uint8_t {
 	UNK_FILE,
@@ -153,6 +154,11 @@ void ImageDownloader::DownloadPic() {
 				}
 				continue;
 			}
+			if (numberImagesDownloaded >= 10) {
+				std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
+				--numberImagesDownloaded;
+			}
+			numberImagesDownloaded++;
 			SetPayloadAndUrl(fmt::format(src.url, code), fp);
 			res = curl_easy_perform(curl);
 			fclose(fp);
